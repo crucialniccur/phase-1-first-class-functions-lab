fb9f9ed2dc09f00b249398572ac1f95b961c923c
Mati Joseph <crucialniccur@gmail.com>
2025-03-04 23:18:29 +0300

    define fare multiplier function

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ const returnLastTwoDrivers = function (drivers) {
 };
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+  return function (fare) {
+    return fare * multiplier;
+  };
+}
